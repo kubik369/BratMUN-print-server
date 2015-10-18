744fccd0a5892b09281952b71828cc47739223ae
Jakub Šimo <kubik0369@gmail.com>
2015-10-18 20:26:35 +0200

    Little changes to Server class

--- src/Server.java
+++ src/Server.java
@@ -7,10 +7,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Server
-{	
-	public static void main(String[] args)
-	{
+public class Server{	
+	public static void main(String[] args){
+		Server server = new Server();
+	}
+	
+	public Server(){
 		String  host = "imap.gmail.com", // change accordingly
 				mailStoreType = "imap";
 		
